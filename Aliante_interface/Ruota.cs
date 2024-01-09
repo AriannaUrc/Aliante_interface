4243afa2484cfdb8043353fecf222dd6ab40ea24
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2024-01-09 13:25:41 +0100

    modifica classe ruota

--- Aliante_interface/Ruota.cs
+++ Aliante_interface/Ruota.cs
@@ -7,94 +7,9 @@ namespace Aliante_interface
 {
     public class Ruota : IComponente
     {
-        private double _costo;
-        private double _lunghezza;
-        private double _altezza;
-        private int _pollici_materiale;
-        private double _raggio;
 
         int dim = 0;
         public List<IComponente> componenti;
-
-
-        public double costo
-        {
-            get { return _costo; }
-            set
-            {
-                if (value < 0)
-                {
-                    _costo = 0;
-                }
-                else
-                {
-                    _costo = value;
-                }
-            }
-        }
-        public double lunghezza
-        {
-            get { return _lunghezza; }
-            set
-            {
-                if (value < 0)
-                {
-                    _lunghezza = 0;
-                }
-                else
-                {
-                    _lunghezza = value;
-                }
-            }
-        }
-
-        public double altezza
-        {
-            get { return _altezza; }
-            set
-            {
-                if (value < 0)
-                {
-                    _altezza = 0;
-                }
-                else
-                {
-                    _altezza = value;
-                }
-            }
-        }
-
-        public int pollici_materiale
-        {
-            get { return _pollici_materiale; }
-            set
-            {
-                if (value < 0)
-                {
-                    _pollici_materiale = 0;
-                }
-                else
-                {
-                    _pollici_materiale = value;
-                }
-            }
-        }
-
-        public double raggio
-        {
-            get { return _raggio; }
-            set
-            {
-                if (value < 0)
-                {
-                    _raggio = 0;
-                }
-                else
-                {
-                    _raggio = value;
-                }
-            }
-        }
 
 
         public Ruota()
